e06568f8136bb167389de085df409bf756a31228
joaaoeu <hello@joaao.eu>
2018-08-12 22:12:57 -0300

    Added hangman game algorithm and notes of leiningen

---
+++ LEIN-NOTES.md
@@ -0,0 +1,24 @@
+# Run core.clj
+```
+$ lein repl
+```
+
+# Reload
+```
+$ (require '[hangman.core :as h] :reload)
+```
+
+# Run -main
+```
+$ lein run
+```
+
+# Create jar
+```
+$ lein uberjar
+```
+
+# Run jar
+```
+$ java -jar archive.jar
+```
--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,7 +1,53 @@
 (ns hangman.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def totalLifes 6)
+(def secretWord "Clojure")
+
+(defn toUppercase [x] (clojure.string/upper-case x))
+
+(defn remainingLetters [word hits]
+  (remove (fn [letter] (contains? hits (str letter))) word))
+
+(defn hitAllWord? [word hits]
+  (empty? (remainingLetters word hits)))
+
+(defn right? [guess word] (.contains word guess))
+
+(defn getHangman [lifes word hits]
+  (println "Lifes:" lifes)
+  (if (> lifes 0)
+    (do
+      (doseq [letter (seq word)]
+        (if (contains? hits (str letter))
+          (print letter " ")
+          (print "_" " ")))
+      (println)
+      (println "Take a guess..."))
+    (do
+      (doseq [letter (seq word)]
+        (print letter " "))
+      (println))))
+
+(defn game [lifes word hits]
+  (getHangman lifes (toUppercase word) hits)
+  (cond
+    (= lifes 0) (println "Game Over!")
+    (hitAllWord? (toUppercase word) hits) (println "You Win!")
+    :else
+    (do
+      (let [guess (read-line)]
+        (if (right? (toUppercase guess) (toUppercase word))
+          (do
+            (println)
+            (println "***** You right! *****")
+            (recur lifes word (conj hits (toUppercase guess))))
+          (do
+            (println)
+            (println "***** You wrong! *****")
+            (recur (dec lifes) word hits)))))))
+
+(defn startGame [] (game totalLifes secretWord #{}))
+
+(defn -main [& args]
+  (startGame))
